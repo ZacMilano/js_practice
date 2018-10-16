0952ca8785268fe6f2bad6c504d80ac0197b8750
Zac Monroe <zmonroe0820@gmail.com>
2018-10-16 14:53:48 -0400

    First file; some node and vim scripts/shortcuts set up

---
+++ first_experimentation.js
@@ -0,0 +1,10 @@
+/* Zac Monroe
+ * first_experimentation.js
+ * File created: Tue 16 Oct 2018 02:50:42 PM EDT
+ * Last changed: Tue 16 Oct 2018 02:53:12 PM EDT
+ */
+
+let somethingSad = "I just spent like 20 minutes making a vim script that " +
+  "dynamically generates the above documentation/comments. I'm not sure if " +
+  "that'd be considered sad or cool. I think it's cool though.";
+console.log(somethingSad);
